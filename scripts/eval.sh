75bc08448ea3b02147a40e6e4ec8ce3bd21c6a01
Wind-2375-like <wind2375like@gmail.com>
2026-05-04 02:08:01 +0000

    olmo 3 7b and llama 3.1 8b

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -1,11 +1,36 @@
+# Add model name parameter
+MODEL_NAME=$1
+
+# Usage: ./scripts/eval.sh <model_name>
+# chmod +x ./scripts/eval.sh first
+
+# Mapping
+# qwen-3-4b -> Qwen_Qwen3-4B
+# qwen-3-8b -> Qwen_Qwen3-8B
+# olmo-3-7b -> allenai_Olmo-3-7B
+# llama-3.1-8b -> meta-llama_Llama-3.1-8B-Instruct
+
+if [ "$MODEL_NAME" == "qwen-3-4b" ]; then
+  MODEL_NAME="Qwen_Qwen3-4B"
+elif [ "$MODEL_NAME" == "qwen-3-8b" ]; then
+  MODEL_NAME="Qwen_Qwen3-8B"
+elif [ "$MODEL_NAME" == "olmo-3-7b" ]; then
+  MODEL_NAME="allenai_Olmo-3-7B-Instruct"
+elif [ "$MODEL_NAME" == "llama-3.1-8b" ]; then
+  MODEL_NAME="meta-llama_Llama-3.1-8B-Instruct"
+else
+  echo "Unknown model name: $MODEL_NAME"
+  exit 1
+fi
+
 python -m eval.run_meta_eval \
-  --sentence-scores results/sentence_scores.Qwen_Qwen3-4B.aggrefact_cnn.parquet \
+  --sentence-scores results/sentence_scores.$MODEL_NAME.aggrefact_cnn.parquet \
   --dataset data/aggrefact_cnn/aggrefact_cnn.parquet
 
 python -m eval.run_meta_eval \
-  --sentence-scores results/sentence_scores.Qwen_Qwen3-4B.diversumm.parquet \
+  --sentence-scores results/sentence_scores.$MODEL_NAME.diversumm.parquet \
   --dataset data/diversumm/diversumm.parquet
 
 python -m eval.run_meta_eval \
-  --sentence-scores results/sentence_scores.Qwen_Qwen3-4B.aggrefact_other_multi.parquet \
+  --sentence-scores results/sentence_scores.$MODEL_NAME.aggrefact_other_multi.parquet \
   --dataset data/aggrefact_other_multi/aggrefact_other_multi.parquet
--- scripts/exp.sh
+++ scripts/exp.sh
@@ -1,14 +1,39 @@
+# Add model name parameter
+MODEL_NAME=$1
+
+# Usage: ./scripts/exp.sh <model_name>
+# chmod +x ./scripts/exp.sh first
+
+# Mapping
+# qwen-3-4b -> Qwen/Qwen3-4B
+# qwen-3-8b -> Qwen/Qwen3-8B
+# olmo-3-7b -> allenai/Olmo-3-7B
+# llama-3.1-8b -> meta-llama/Llama-3.1-8B-Instruct
+
+if [ "$MODEL_NAME" == "qwen-3-4b" ]; then
+  MODEL_NAME="Qwen/Qwen3-4B"
+elif [ "$MODEL_NAME" == "qwen-3-8b" ]; then
+  MODEL_NAME="Qwen/Qwen3-8B"
+elif [ "$MODEL_NAME" == "olmo-3-7b" ]; then
+  MODEL_NAME="allenai/Olmo-3-7B-Instruct"
+elif [ "$MODEL_NAME" == "llama-3.1-8b" ]; then
+  MODEL_NAME="meta-llama/Llama-3.1-8B-Instruct"
+else
+  echo "Unknown model name: $MODEL_NAME"
+  exit 1
+fi
+
 python scripts/score_sentences.py \
     --dataset-path data/aggrefact_cnn/aggrefact_cnn.parquet \
     --dataset-name aggrefact_cnn \
-    --model Qwen/Qwen3-4B
+    --model $MODEL_NAME
 
 python scripts/score_sentences.py \
     --dataset-path data/diversumm/diversumm.parquet \
     --dataset-name diversumm \
-    --model Qwen/Qwen3-4B
+    --model $MODEL_NAME
 
 python scripts/score_sentences.py \
     --dataset-path data/aggrefact_other_multi/aggrefact_other_multi.parquet \
     --dataset-name aggrefact_other_multi \
-    --model Qwen/Qwen3-4B
+    --model $MODEL_NAME
